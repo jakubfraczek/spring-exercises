ef97b1f0166a8914b6685498051d841a6578fab5
Jakub Frączek <kuba.fraczek@gmail.com>
2017-03-11 09:42:42 +0100

    surowy projekt

---
+++ spring-basics-2/src/main/java/pl/sda/beans/Bean.java
@@ -0,0 +1,14 @@
+package pl.sda.beans;
+
+public class Bean {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
